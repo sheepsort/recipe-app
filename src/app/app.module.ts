85e9111152d73d68d941df5e910650e9c68c2a59
JG930731 <JG930731@RDI-JGRIFFITH>
2019-09-09 14:31:54 -0500

    setting up bootstrap and scaffolding out the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe_book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe_book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping_list/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping_list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
