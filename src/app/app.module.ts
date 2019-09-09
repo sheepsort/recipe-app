a20a4d67ae3ad1a2de6c95b96f0680d1d8156415
JG930731 <JG930731@RDI-JGRIFFITH>
2019-09-09 15:06:33 -0500

    built nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipeBookComponent } from './recipe_book/recipe-book.component';
 import { RecipeListComponent } from './recipe_book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe_book/recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe_book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe_book/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping_list/shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping_list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListEditComponent } from './shopping_list/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipeBookComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
